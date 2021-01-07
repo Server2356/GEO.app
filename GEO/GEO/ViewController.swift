d760ac1dc43f2bb0f119d03eb11682d554442c36
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2021-01-07 16:44:20 +0000

    Adds new GEO 1.1 app improvements

---
+++ GEO/GEO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GEO
+//
+//  Created by Abdon Morales Jr on 11/20/20.
+//
+
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    var uiDelegate: WKUIDelegate?
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://nypost.com/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }}
